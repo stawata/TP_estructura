9f5751ae10b5578014cc65b2c3fa704b95dfd4b3
bautistefano <bstefano@itba.edu.ar>
2025-06-03 19:06:03 -0300

    a

---
+++ vehiculos.py
@@ -0,0 +1,13 @@
+class Vehiculo():
+    def __init__(self,modo,velocidad,capacidad,costo_fijo,costo_km,costo_kg):
+       self.modo=modo
+       self.velocidad=velocidad
+       self.capacidad=capacidad
+       self.costo_fijo=costo_fijo
+       self.costo_km=costo_km
+       self.costo_kg=costo_kg
+      
+    def __str__(Self):
+        return f"{Self.nombre}"
+    
+   
