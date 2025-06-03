50d5f807776dcf568e90a3f476e753a7a7730bee
StefaniaSantullli <ssantulli@itba.edu.ar>
2025-06-03 19:23:49 -0300

    modificacion_conexion

---
+++ vehiculos_herencia.py
@@ -0,0 +1,14 @@
+from vehiculos import Vehiculo
+
+class Camion(Vehiculo):
+    pass
+
+class Tren():
+    pass
+
+class Avion():
+    #aca hay un metodo en caso de malas condiciones climaticas
+    pass
+
+class Barcaza():
+    pass
