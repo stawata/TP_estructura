be6d44ae72a99ca51e972eb68b320184222cf79a
bautistefano <bstefano@itba.edu.ar>
2025-06-03 19:10:34 -0300

    a

--- vehiculos.py
+++ vehiculos.py
@@ -7,7 +7,17 @@ class Vehiculo():
        self.costo_km=costo_km
        self.costo_kg=costo_kg
       
-    def __str__(Self):
-        return f"{Self.nombre}"
+    def __str__(self):
+        return f"{self.nombre}"
     
-   
+    def calcular_tiempo():
+        pass
+        """Calcula el tiempo que tarda el vehículo en recorrer una cierta distancia (en km)."""
+    
+    def calcular_costo():
+        pass
+        """Calcula el costo total de utilizar el vehículo en un tramo, dado una distancia y una cantidad de carga."""
+
+    def puede_usar_conexion():
+        pass
+        """Evalúa si el vehículo puede usar una conexión determinada, según las restricciones propias del vehículo y del tramo"""
