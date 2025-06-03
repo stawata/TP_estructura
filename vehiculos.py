42c3126bb4c0a87022ea2771a050fafeab35390c
bautistefano <bstefano@itba.edu.ar>
2025-06-03 19:36:54 -0300

    Vehiculo completa

--- vehiculos.py
+++ vehiculos.py
@@ -1,23 +1,34 @@
+import math
+
 class Vehiculo():
-    def __init__(self,modo,velocidad,capacidad,costo_fijo,costo_km,costo_kg):
+    def __init__(self,modo,velocidad_nominal,capacidad,costo_fijo,costo_km,costo_kg):
        self.modo=modo
-       self.velocidad=velocidad
+       self.velocidad_nominal=velocidad_nominal
        self.capacidad=capacidad
        self.costo_fijo=costo_fijo
        self.costo_km=costo_km
        self.costo_kg=costo_kg
       
     def __str__(self):
-        return f"{self.nombre}"
+        return f"Este vehículo tiene el modo: {self.modo}"
     
-    def calcular_tiempo():
-        pass
-        """Calcula el tiempo que tarda el vehículo en recorrer una cierta distancia (en km)."""
-    
-    def calcular_costo():
-        pass
-        """Calcula el costo total de utilizar el vehículo en un tramo, dado una distancia y una cantidad de carga."""
+    def cantidad_necesaria(self,peso):
+        return math.ceil(peso/self.capacidad)
+        """Aca ves segun la capacidad del vehiculo cuantas unidades necesitas del mismo, nos sirve para calcular el costo en las hijas"""
 
-    def puede_usar_conexion():
-        pass
+    def calcular_tiempo(self,distancia):
+        raise NotImplementedError("Este método debe ser implementado por las subclases.")
+        """Calcula el tiempo que tarda el vehículo en recorrer una cierta distancia (en km)."""
+        """Acá que tener cuidado con las condiciones especiales de cada uno para esta funcion"""
+    
+    def calcular_costo(self,distancia,peso):
+        raise NotImplementedError("Este método debe ser implementado por las subclases.")
+        """Calcula el costo total de utilizar el vehículo en un tramo, dado una distancia y una cantidad de carga."""
+        
+    def puede_usar_conexion(self):
+        raise NotImplementedError("Este método debe ser implementado por las subclases.")
         """Evalúa si el vehículo puede usar una conexión determinada, según las restricciones propias del vehículo y del tramo"""
+
+
+#Esta clase trabaja con Conexion para validar si puede recorrerla en la funcion "puede_usar_conexion"
+#Ademas, con Solicitud para ver si puede cumplir el pedido y uso peso
