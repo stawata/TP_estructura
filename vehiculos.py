43f00c4b6deb5f6041993ae04c692b25e46c13c0
bautistefano <bstefano@itba.edu.ar>
2025-06-03 19:37:20 -0300

    a

--- vehiculos.py
+++ vehiculos.py
@@ -13,21 +13,25 @@ class Vehiculo():
         return f"Este vehículo tiene el modo: {self.modo}"
     
     def cantidad_necesaria(self,peso):
-        return math.ceil(peso/self.capacidad)
         """Aca ves segun la capacidad del vehiculo cuantas unidades necesitas del mismo, nos sirve para calcular el costo en las hijas"""
-
+        
+        return math.ceil(peso/self.capacidad)
+        
     def calcular_tiempo(self,distancia):
-        raise NotImplementedError("Este método debe ser implementado por las subclases.")
         """Calcula el tiempo que tarda el vehículo en recorrer una cierta distancia (en km)."""
         """Acá que tener cuidado con las condiciones especiales de cada uno para esta funcion"""
-    
-    def calcular_costo(self,distancia,peso):
+        
         raise NotImplementedError("Este método debe ser implementado por las subclases.")
+        
+    def calcular_costo(self,distancia,peso):
         """Calcula el costo total de utilizar el vehículo en un tramo, dado una distancia y una cantidad de carga."""
         
-    def puede_usar_conexion(self):
         raise NotImplementedError("Este método debe ser implementado por las subclases.")
+        
+    def puede_usar_conexion(self):
         """Evalúa si el vehículo puede usar una conexión determinada, según las restricciones propias del vehículo y del tramo"""
+        
+        raise NotImplementedError("Este método debe ser implementado por las subclases.")
 
 
 #Esta clase trabaja con Conexion para validar si puede recorrerla en la funcion "puede_usar_conexion"
