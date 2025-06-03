cd35cd405c95e09e196c69b0772527694f48af01
bautistefano <bstefano@itba.edu.ar>
2025-06-03 19:51:04 -0300

    Primer herencia

--- vehiculos_herencia.py
+++ vehiculos_herencia.py
@@ -1,7 +1,9 @@
 from vehiculos import Vehiculo
 
 class Camion(Vehiculo):
-    pass
+    def __init__(self, modo, velocidad_nominal, capacidad, costo_fijo, costo_km, costo_kg):
+        super().__init__(modo, velocidad_nominal, capacidad, costo_fijo, costo_km, costo_kg)
+    
 
 class Tren():
     pass
