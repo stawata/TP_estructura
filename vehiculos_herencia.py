22a6f8be17886be085b020a80d4018d609215e51
StefaniaSantullli <ssantulli@itba.edu.ar>
2025-06-03 20:11:43 -0300

    prueba herencia

--- vehiculos_herencia.py
+++ vehiculos_herencia.py
@@ -1,8 +1,8 @@
 from vehiculos import Vehiculo
 
 class Camion(Vehiculo):
-    def __init__(self, modo, velocidad_nominal, capacidad, costo_fijo, costo_km, costo_kg):
-        super().__init__(modo, velocidad_nominal, capacidad, costo_fijo, costo_km, costo_kg)
+    def __init__(self):
+        super().__init__("automor", 80, 30000, 30, 5, 1)
 
 class Tren(Vehiculo):
     def __init__(self, modo, velocidad_nominal, capacidad, costo_fijo, costo_km, costo_kg):
@@ -12,6 +12,8 @@ class Avion(Vehiculo):
     #aca hay un metodo en caso de malas condiciones climaticas
     def __init__(self, modo, velocidad_nominal, capacidad, costo_fijo, costo_km, costo_kg):
         super().__init__(modo, velocidad_nominal, capacidad, costo_fijo, costo_km, costo_kg)
+
+    
 
 class Barcaza(Vehiculo):
     def __init__(self, modo, velocidad_nominal, capacidad, costo_fijo, costo_km, costo_kg):
