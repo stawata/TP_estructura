7b7ba6e0680896c39e7edc3cc7797c6b3e50a516
bautistefano <bstefano@itba.edu.ar>
2025-06-03 20:25:00 -0300

    a

--- vehiculos_herencia.py
+++ vehiculos_herencia.py
@@ -1,8 +1,11 @@
 from vehiculos import Vehiculo
+from clase_solicitud import Solicitud
 
 class Camion(Vehiculo):
-    def __init__(self):
-        super().__init__("automor", 80, 30000, 30, 5, 1)
+    def __init__(self,costo_kg):
+        #El costo_kg varia segun la carga de la solicitud, ver como hacer
+        super().__init__(modo="automotor",velocidad_nominal= 80,capacidad= 30000,costo_fijo= 30,costo_km= 5,costo_kg=costo_kg)
+        
 
 class Tren(Vehiculo):
     def __init__(self, modo, velocidad_nominal, capacidad, costo_fijo, costo_km, costo_kg):
