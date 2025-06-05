3ab69008314264b5b988712325a5867f7644da78
Belen <mpiumajusto@itba.edu.ar>
2025-06-05 18:52:37 -0300

    agrego clase pila

---
+++ pilas.py
@@ -0,0 +1,18 @@
+class Pila():
+    def __init__(self):
+        self.rutas=[]
+
+    def esta_vacia(self):
+        return len(self.rutas) == 0
+    
+    def apilar(self,ruta):
+        self.rutas.append(ruta)
+
+    def cima(self):
+        if self.esta_vacia():
+            raise IndexError("La pila está vacía")
+        return self.rutas[-1]
+
+    def mostrar(self):
+        print("Pila:", self.rutas[::-1]) 
+
