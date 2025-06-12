b45591c5ee967a5f7fbe631d43ffc4306e3ce285
bautistefano <bstefano@itba.edu.ar>
2025-06-12 18:43:46 -0300

    Modularizamos y pasamos de una lista de lista a un diccionario

--- main.py
+++ main.py
@@ -30,20 +30,11 @@ try:
             for lista in ruta:
                 if lista[0] not in grafo: 
                     grafo[lista[0]] =  [lista[1:]]
-                    if lista[1] not in grafo:
-                        inverso = lista[2:]
-                        inverso.insert(0, lista[0])  
-                        grafo[lista[1]] = [inverso]  
-                    else:
-                        grafo[lista[1]].append([lista[i] for i in indices if i < len(lista)])
+                    self.agregar_conexion(grafo, lista[0], lista[1], [lista[i] for i in indices if i < len(lista)])
                 else:
                     grafo[lista[0]].append(lista[1:])
-                    if lista[1] not in grafo:
-                        inverso = lista[2:]
-                        inverso.insert(0, lista[0])  
-                        grafo[lista[1]] = [inverso]   
-                    else:
-                        grafo[lista[1]].append([lista[i] for i in indices if i < len(lista)])
+                    self.agregar_conexion(grafo, lista[0], lista[1], [lista[i] for i in indices if i < len(lista)])
+
             return grafo
 
         def ruta_optima(self,solicitud,recorrido, modo):
@@ -115,6 +106,15 @@ try:
             for valor in resultado[0]:
                 print(f"La ciudad es: {valor.nombre}")
 
+        @staticmethod
+        def agregar_conexion(grafo, origen, destino, datos):
+            if destino not in grafo:
+                inverso = datos[1:]
+                inverso.insert(0, origen)
+                grafo[destino] = [inverso]
+            else:
+                grafo[destino].append(datos)
+            
 
 
             
