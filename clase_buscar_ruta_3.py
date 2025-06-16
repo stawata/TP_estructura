174bdd60cc41ae27fddb19f22a8260fc13df7d1d
Belen <mpiumajusto@itba.edu.ar>
2025-06-16 01:34:49 -0300

    cambios en clase buscar ruta

--- clase_buscar_ruta_3.py
+++ clase_buscar_ruta_3.py
@@ -1,16 +1,20 @@
 from pilas import Pila
+from models.solicitud import Solicitud 
+from utils.grafos import *
+from models.vehiculos_herencia import *
 
+tipos_vehiculo={"ferroviaria":Tren, "automotor":Camion, "aerea":Avion,"fluvial":Barcaza}
 class Buscar_ruta:
-    def __init__(self,conexiones):
-        self.conexiones=conexiones #guarda la lista de objetos conexion
-
+    def __init__(self,grafo,tipo_vehiculo):
+        self.grafo=grafo #guarda la lista de objetos conexion
+        self.vehiculo=tipos_vehiculo[tipo_vehiculo]
     def buscar_caminos(self,solicitud): #Guarda desde solicitud el origen y destino
         #ACA TENDRIAMOS QUE TRABAJAR SOBRE EL OBJETO DE UNA CLASE 
-        origen=solicitud[2]
-        destino=solicitud[3]
+        origen=solicitud.getorigen()
+        destino=solicitud.getdestino()
 
         caminos_encontrados=[] #aca se van a guardar las alternativas de camino
-        pila=Pila()
+        pila=Pila()                    #Cada entrada en la pila es una tupla: la ciudad actual y el camino recorrido hasta ahora
         pila.apilar((origen,[origen])) #origen nos muestra donde estamos parados actualmente y [origen] es la lista con el camino recorrido
 
         while  not pila.esta_vacia():  #mientras que haya caminos posibles (la pila no este vacia) seguimos buscando
@@ -20,40 +24,46 @@ class Buscar_ruta:
                 caminos_encontrados.append(camino) #Cuando encuentra un nodo igual al destino guarda el camino
                 continue
 
-            for conexion in self.conexiones:
-                #ACA TENDRIAMOS QUE TAMBIEN TRABAJAR SOBRE UN OBJETO
-                ciudad_origen=conexion[0]
-                ciudad_destino=conexion[1]
-                if ciudad_origen==nodo_actual and ciudad_destino not in camino: #agregamos otro tramo de camino por que todavia no se llego al destino
-                    nuevo_camino=camino+[ciudad_destino]
-                    pila.apilar((ciudad_destino, nuevo_camino))
+            for vecino,distancia, otro_valor in self.grafo.get(nodo_actual,[]):
+                if vecino not in camino:   #evita ciclos
+                    nuevo_camino=camino+[vecino]
+                    pila.apilar((vecino, nuevo_camino))
 
                 #con este de abajo revisas el camino de manera inversa 
 
-                if ciudad_destino == nodo_actual and ciudad_origen not in camino:
-                    nuevo_camino = camino + [ciudad_origen]
-                    pila.apilar((ciudad_origen, nuevo_camino))
+                #if ciudad_destino == nodo_actual and ciudad_origen not in camino:
+                #    nuevo_camino = camino + [ciudad_origen]
+                #    pila.apilar((ciudad_origen, nuevo_camino))
+            #calcular tiempos y costos
+            resultados=[]
+            for camino in caminos_encontrados:
+                tiempo_total=0
+                costo_total=0
+                for i in range(len(camino)-1):
+                    ciudad1=camino[i]
+                    ciudad2=camino[i+1]
+                    for vecino, distancia, velocidad in self.grafo[ciudad1]:
+                        if vecino==ciudad2:
+                            tiempo_total+=self.vehiculo.calcular_tiempo(distancia,vecino)
 
         return caminos_encontrados
 
 
 #esto de aca es una prueba luego se borra
 
-ruta = [
-    ["Zarate", "Buenos_Aires", "Automotor", 85,  ""],
-    ["Zarate", "Junin", "Automotor", 185, "peso_max", 15000],
-    ["Junin", "Buenos_Aires", "Automotor", 238,  ""],
-    ["Junin", "Azul", "Automotor", 265,  ""],
-    ["Azul", "Buenos_Aires", "Automotor", 278,  ""],
-    ["Azul", "Mar_del_Plata", "Automotor", 246,  ""],
-    ["Buenos_Aires", "Mar_del_Plata", "Automotor", 384,  ""],
-]
+grafo_info = {
+     "Zarate": [("Buenos Aires", 85, 100), ("Junin", 185, 80)],
+     "Buenos Aires": [("Zarate", 85, 100), ("Junin", 238, 90), ("Azul", 278, 110), ("Mar del Plata", 384, 120)],
+     "Junin": [("Zarate", 185, 80), ("Buenos Aires", 238, 90), ("Azul", 265, 100)],
+     "Azul": [("Junin", 265, 100), ("Buenos Aires", 278, 110), ("Mar del Plata", 246, 100)],
+     "Mar del Plata": [("Buenos Aires", 384, 120), ("Azul", 246, 100)]
+ }
 
 vehiculos={"Automotor", "Ferroviaria"}
 
-solicitud = ["CARGA_001",70000,"Zarate","Mar_del_Plata"]
+solicitud = Solicitud("CARGA_001",70000,"Zarate","Mar_del_Plata")
 
-buscador = Buscar_ruta(ruta)
+buscador = Buscar_ruta(grafo_info)
 caminos = buscador.buscar_caminos(solicitud)
 
 # Mostrar resultados
