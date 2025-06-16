9f5d99f8d2fe217c3f9ed7fa2f81d1548e80321a
Belen <mpiumajusto@itba.edu.ar>
2025-06-16 09:25:45 -0300

    cambios

--- itinerario/buscar_ruta.py
+++ itinerario/buscar_ruta.py
@@ -12,6 +12,7 @@ class Buscar_ruta:
         #ACA TENDRIAMOS QUE TRABAJAR SOBRE EL OBJETO DE UNA CLASE 
         origen=solicitud.getorigen()
         destino=solicitud.getdestino()
+        peso=solicitud.getpeso( )
 
         caminos_encontrados=[] #aca se van a guardar las alternativas de camino
         pila=Pila()                    #Cada entrada en la pila es una tupla: la ciudad actual y el camino recorrido hasta ahora
@@ -21,7 +22,17 @@ class Buscar_ruta:
             nodo_actual,camino=pila.desapilar() #nos da la ciudad donde estemos parados y compara si ya es el destino
 
             if nodo_actual==destino:
-                caminos_encontrados.append(camino) #Cuando encuentra un nodo igual al destino guarda el camino
+                #calculat costo y tiempo
+                tiempo_total=0
+                costo_total=0
+                for i in range(len(camino)-1):
+                    ciudad1=camino[i]
+                    ciudad2=camino[i+1]
+                    for vecino, distancia, restriccion in self.grafo[ciudad1]:
+                        if vecino==ciudad2:
+                            tiempo_total+=self.vehiculo.calcular_tiempo(distancia,vecino,restriccion)
+                            costo_total+=self.vehiculo.calcular_costo(distancia,peso)
+                caminos_encontrados.append({"camino": camino,"tiempo_total":round(tiempo_total,2),"costo_total":round(costo_total,2),"vehiculo":self.vehiculo.__name__}) #Cuando encuentra un nodo igual al destino guarda el camino
                 continue
 
             for vecino,distancia, otro_valor in self.grafo.get(nodo_actual,[]):
@@ -35,19 +46,22 @@ class Buscar_ruta:
                 #    nuevo_camino = camino + [ciudad_origen]
                 #    pila.apilar((ciudad_origen, nuevo_camino))
             #calcular tiempos y costos
-            resultados=[]
-            for camino in caminos_encontrados:
-                tiempo_total=0
-                costo_total=0
-                for i in range(len(camino)-1):
-                    ciudad1=camino[i]
-                    ciudad2=camino[i+1]
-                    for vecino, distancia, velocidad in self.grafo[ciudad1]:
-                        if vecino==ciudad2:
-                            tiempo_total+=self.vehiculo.calcular_tiempo(distancia,vecino)
 
         return caminos_encontrados
 
+    def mostrar_resultados(caminos_encontrados):
+        print(f"{'Solución':<10} {'Modo':<12} {'Itinerario':<50} {'Costo total':>15} {'Tiempo total [min]':>20}")
+        print("-" * 110)
+
+        letra = ord('A')
+        for camino in caminos_encontrados:
+            modo = camino["vehiculo"]
+            itinerario = " - ".join(camino["camino"])
+            costo = f"${camino['costo_total']:.3f}"
+            tiempo = int(camino["tiempo_total"])  # ver si hay que pasar a horas
+
+            print(f"{chr(letra):<10} {modo:<12} {itinerario:<50} {costo:>15} {tiempo:>20}")
+            letra += 1
 #esto de aca es una prueba luego se borra
 
 grafo_info = {
@@ -62,11 +76,10 @@ vehiculos={"Automotor", "Ferroviaria"}
 
 solicitud = Solicitud("CARGA_001",70000,"Zarate","Mar_del_Plata")
 
-buscador = Buscar_ruta(grafo_info)
+buscador = Buscar_ruta(grafo_info,"ferroviaria")
 caminos = buscador.buscar_caminos(solicitud)
 
 # Mostrar resultados
-for i, camino in enumerate(caminos):
-    print(f"Camino {i+1}: {' → '.join(camino)}")
+buscador.mostrar_resultados(caminos)
 
 
