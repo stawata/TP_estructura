6a539b13b5843b90e805ce1f9a426a99946484c9
bautistefano <bstefano@itba.edu.ar>
2025-06-17 18:46:20 -0300

    Borrado dijstra que no sirve

--- algoritmo_disjtra/Nodo_ciudad.py
+++
@@ -1,6 +0,0 @@
-class NodoCiudad:
-    def __init__(self, nombre, tiempo, previo):
-        self.nombre = nombre
-        self.tiempo = tiempo
-        self.previo = previo
-        self.siguiente = None
