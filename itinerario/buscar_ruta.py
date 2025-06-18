87fbe463c8c5e207d283228b13b21d266ad41881
bautistefano <bstefano@itba.edu.ar>
2025-06-18 20:49:28 -0300

    a

--- itinerario/buscar_ruta.py
+++ itinerario/buscar_ruta.py
@@ -68,7 +68,7 @@ class Buscar_ruta:
                 modo = camino["vehiculo"].__name__
                 itinerario = " - ".join(camino["camino"])
                 costo = f"${camino['costo_total']:.3f}"
-                tiempo = int(camino["tiempo_total"])
+                tiempo = round(int(camino["tiempo_total"]),2)
 
                 print(f"{chr(letra):<10} {modo:<12} {itinerario:<50} {costo:>15} {tiempo:>20}")
                 letra += 1
